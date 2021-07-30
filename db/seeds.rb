8a73f1e75fb450b5742e08c40bef405f5b327f27
Tom Chen <chen.thomas.c@gmail.com>
2021-07-29 21:59:56 -0400

    add filings to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+filing_urls = [
+  "http://s3.amazonaws.com/irs-form-990/201132069349300318_public.xml",
+  "http://s3.amazonaws.com/irs-form-990/201612429349300846_public.xml",
+  "http://s3.amazonaws.com/irs-form-990/201521819349301247_public.xml",
+  "http://s3.amazonaws.com/irs-form-990/201641949349301259_public.xml",
+  "http://s3.amazonaws.com/irs-form-990/201921719349301032_public.xml",
+  "http://s3.amazonaws.com/irs-form-990/201831309349303578_public.xml",
+  "http://s3.amazonaws.com/irs-form-990/201823309349300127_public.xml",
+  "http://s3.amazonaws.com/irs-form-990/201401839349300020_public.xml",
+  "http://s3.amazonaws.com/irs-form-990/201522139349100402_public.xml",
+  "http://s3.amazonaws.com/irs-form-990/201831359349101003_public.xml"
+]
+
+puts "Seeding filings..."
+
+filing_urls.each do |url|
+  uri = URI(url)
+  xml_string = Net::HTTP.get(uri)
+  xml_document = Nokogiri::XML(xml_string.squish)
+
+  Filing.create(url: url, data: xml_document)
+end
+
+puts "Filings seeded!"
