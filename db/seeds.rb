5911f17805744e86fc6ed20b0e4d634af995d86f
Tom Chen <chen.thomas.c@gmail.com>
2021-07-31 22:14:08 -0400

    fix up seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,51 +19,58 @@ filing_urls = [
   "http://s3.amazonaws.com/irs-form-990/201831359349101003_public.xml"
 ]
 
-puts "Seeding filings..."
+puts "Creating filings..."
 
 filing_urls.each do |url|
   uri = URI(url)
   xml_string = Net::HTTP.get(uri)
   xml_document = Nokogiri::XML(xml_string.squish)
   
-  # Filing.create!(url: url, data: xml_document)
-  
-  # filer_fragment = Nokogiri::XML.fragment(xml_document.xpath("//irs:Return//irs:ReturnHeader//irs:Filer", "irs" => "http://www.irs.gov/efile"))
-  # filer_attributes = {
-  #   ein: filer_fragment.at(".//EIN").text.squish,
-  #   name: filer_fragment.at(".//*[contains(name(), 'Name')]").text.squish,
-  #   address: filer_fragment.xpath(".//*[starts-with(name(), 'AddressLine')]").text.squish,
-  #   city: filer_fragment.at(".//*[starts-with(name(),'City')]").text.squish,
-  #   state: filer_fragment.at(".//*[starts-with(name(),'State')]").text.squish,
-  #   zip: filer_fragment.at(".//*[starts-with(name(),'ZIP')]").text.squish
-  # }
-  # Filer.create_with(
-  #   name: filer_attributes[:name],
-  #   address: filer_attributes[:address],
-  #   city: filer_attributes[:city],
-  #   state: filer_attributes[:state],
-  #   zip: filer_attributes[:zip]
-  # ).find_or_create_by(ein: filer_attributes[:ein])
+  filer_fragment = Nokogiri::XML.fragment(xml_document.xpath("//irs:Return//irs:ReturnHeader//irs:Filer", "irs" => "http://www.irs.gov/efile"))
+  filer = Filer.create_with(
+    name: filer_fragment.at(".//*[contains(name(), 'Name')]").text.squish,
+  ).find_or_create_by!(ein: filer_fragment.at(".//EIN").text.squish)
+  filer_address = Address.find_or_create_by!(
+    street: filer_fragment.xpath(".//*[starts-with(name(), 'AddressLine')]").text.squish,
+    city: filer_fragment.at(".//*[starts-with(name(),'City')]").text.squish,
+    state: filer_fragment.at(".//*[starts-with(name(),'State')]").text.squish,
+    zip: filer_fragment.at(".//*[starts-with(name(),'ZIP')]").text.squish
+  )
 
-  # puts filer_attributes
+  filing = Filing.create!(url: url, xml: xml_document, filer: filer, filer_address: filer_address)
 
   receivers = xml_document.xpath("//irs:Return//irs:ReturnData//irs:IRS990ScheduleI/irs:RecipientTable", "irs" => "http://www.irs.gov/efile")
-  results = []
   receivers.each do |receiver|
     fragment = Nokogiri::XML.fragment(receiver)
 
-    receiver_attributes = {
-      ein: fragment.at(".//*[contains(name(), 'EIN')]")&.text&.squish,
-      name: fragment.at(".//*[contains(name(),'Name')]")&.text&.squish,
-      address: fragment.xpath(".//*[starts-with(name(), 'AddressLine')]")&.text&.squish,
-      city: fragment.at(".//*[starts-with(name(),'City')]")&.text&.squish,
-      state: fragment.at(".//*[starts-with(name(),'State')]")&.text&.squish,
-      zip: fragment.at(".//*[starts-with(name(),'ZIP')]")&.text&.squish
-    }
+    receiver_address = Address.find_or_create_by!(
+      street: fragment.xpath(".//*[starts-with(name(), 'AddressLine')]").text.squish,
+      city: fragment.at(".//*[starts-with(name(),'City')]").text.squish,
+      state: fragment.at(".//*[starts-with(name(),'State')]").text.squish,
+      zip: fragment.at(".//*[starts-with(name(),'ZIP')]").text.squish
+    )
 
-    results << receiver_attributes
+    receiver = if fragment.at(".//*[contains(name(), 'EIN')]")
+      Receiver.create_with(
+        name: fragment.at(".//*[contains(name(),'Name')]").text.squish,
+      ).find_or_create_by!(ein: fragment.at(".//*[contains(name(), 'EIN')]").text.squish)
+    else
+      Receiver.find_or_create_by!(name: fragment.at(".//*[contains(name(),'Name')]").text.squish)
+    end
+
+    Award.create!(
+      cash_amount: fragment.at(".//*[contains(name(),'Cash')]").text.squish,
+      purpose: fragment.at(".//*[contains(name(),'Purpose')]").text.squish,
+      filer: filer,
+      filing: filing,
+      receiver: receiver,
+      receiver_address: receiver_address
+    )
   end
-  puts results.sort_by { |x| x[:name] }.select { |x| x[:name] == "Pasadena Conservatory of Music" || x[:name] == "Doctors Without Borders" }
 end
 
-puts "Filings seeded!"
+puts "#{Filing.count} filings created!"
+puts "#{Filer.count} filers created!"
+puts "#{Address.count} addresses created!"
+puts "#{Receiver.count} receivers created!"
+puts "#{Award.count} awards created!"
